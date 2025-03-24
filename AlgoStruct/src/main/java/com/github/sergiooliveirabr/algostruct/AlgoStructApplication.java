ca10bebf9eace615bd43bb6ec6d17c7eff31f4bc
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-24 11:00:57 +0000

    commence

---
+++ AlgoStruct/src/main/java/com/github/sergiooliveirabr/algostruct/AlgoStructApplication.java
@@ -0,0 +1,13 @@
+package com.github.sergiooliveirabr.algostruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoStructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoStructApplication.class, args);
+	}
+
+}
---
+++ AlgoStruct/src/test/java/com/github/sergiooliveirabr/algostruct/AlgoStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sergiooliveirabr.algostruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoStructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
