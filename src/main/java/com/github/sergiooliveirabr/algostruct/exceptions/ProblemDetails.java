3df1a2da2cba962ecf3c0676eb078428cae22dcd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-29 21:29:07 +0000

    Create ProblemDetails.java

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/exceptions/ProblemDetails.java
@@ -0,0 +1,12 @@
+package com.github.sergiooliveirabr.algostruct.exceptions;
+
+public final class ProblemDetails {
+
+    private final int status;
+    private final String message;
+
+    public ProblemDetails(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
