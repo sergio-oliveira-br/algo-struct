0c5439e80cb3ca73fbfe5c9d874d9d67beeb206a
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-29 21:44:00 +0000

    Update RandomNumController.java
    act:
    -> test concluded - returning for the correct version (clean up)

--- src/main/java/com/github/sergiooliveirabr/algostruct/controller/RandomNumController.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/controller/RandomNumController.java
@@ -9,35 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 
 @RestController
 @RequestMapping("/random-num")
 public class RandomNumController {
 
     private final RandomNumService randomNumService;
-    private final BubbleSortService bubbleSortService;
 
     @Autowired
-    public RandomNumController(RandomNumService randomNumService,
-                               BubbleSortService bubbleSortService) {
+    public RandomNumController(RandomNumService randomNumService) {
         this.randomNumService = randomNumService;
-        this.bubbleSortService = bubbleSortService;
     }
 
     @GetMapping("/generate")
     public ResponseEntity<String> getRandomNum(int qty) {
 
-        System.out.println("Starting the generation of " + qty + " random numbers");
-        int [] myData = randomNumService.generateRandomNum(qty);
-
-        System.out.println("Sorting");
-        bubbleSortService.bubbleSort(myData);
-
-        for (int i = 0; i < qty; i++) {
-            System.out.println(myData[i]);
-        }
-
+        randomNumService.generateRandomNum(qty);
         return ResponseEntity.status(HttpStatus.CREATED).body(qty + " numbers genereted successfully");
     }
 }
