8fc65755af9c3edc446635f745b91daf2a4b14f9
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-02 19:11:29 +0100

    Update ElapsedTimeService.java
    act:
    -> approach changed

--- src/main/java/com/github/sergiooliveirabr/algostruct/service/ElapsedTimeService.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/ElapsedTimeService.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ElapsedTimeService {
 
-    long elapsedTime;
-
-    public Long getElapsedTime(long endTime, long startTime) {
-
-        elapsedTime = endTime - startTime;
+    public long formatElapsedTime(long elapsedTime) {
         return elapsedTime;
     }
 }
