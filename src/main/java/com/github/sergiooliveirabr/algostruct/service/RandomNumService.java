10af2472b0bc30344a98411ca295f6ba330e80c5
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-02 23:14:10 +0100

    Update RandomNumService.java
    cleanup

--- src/main/java/com/github/sergiooliveirabr/algostruct/service/RandomNumService.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/RandomNumService.java
@@ -1,6 +1,5 @@
 package com.github.sergiooliveirabr.algostruct.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
