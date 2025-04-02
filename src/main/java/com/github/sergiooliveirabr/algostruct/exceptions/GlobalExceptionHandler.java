4819d16c65ed0683208077f84b960eac895b45cc
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-02 19:13:17 +0100

    Update GlobalExceptionHandler.java
    Act:
    -> disabled

--- src/main/java/com/github/sergiooliveirabr/algostruct/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/exceptions/GlobalExceptionHandler.java
@@ -1,26 +1,26 @@
-package com.github.sergiooliveirabr.algostruct.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-    private ResponseEntity<ProblemDetails> buildErrorResponse (HttpStatus status, String message) {
-
-        ProblemDetails problemDetails = new ProblemDetails(
-                status.value(),
-                message
-        );
-        return new ResponseEntity<>(problemDetails, status);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ProblemDetails> exception (Exception e) {
-
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return buildErrorResponse(status, e.getMessage());
-    }
-}
+//package com.github.sergiooliveirabr.algostruct.exceptions;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    private ResponseEntity<ProblemDetails> buildErrorResponse (HttpStatus status, String message) {
+//
+//        ProblemDetails problemDetails = new ProblemDetails(
+//                status.value(),
+//                message
+//        );
+//        return new ResponseEntity<>(problemDetails, status);
+//    }
+//
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<ProblemDetails> exception (Exception e) {
+//
+//        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+//        return buildErrorResponse(status, e.getMessage());
+//    }
+//}
