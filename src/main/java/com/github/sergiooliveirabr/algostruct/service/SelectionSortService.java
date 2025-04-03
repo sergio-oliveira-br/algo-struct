71f2a08382eee900bb401d8cfc02ec781a4e8dfe
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-03 11:44:27 +0100

    Create SelectionSortService.java
    Act:
    -> Method implemented

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/SelectionSortService.java
@@ -0,0 +1,43 @@
+package com.github.sergiooliveirabr.algostruct.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SelectionSortService implements SortingAlgorithm {
+
+    private final ElapsedTimeService elapsedTimeService;
+
+    @Autowired
+    public SelectionSortService(ElapsedTimeService elapsedTimeService) {
+        this.elapsedTimeService = elapsedTimeService;
+    }
+
+    @Override
+    public long sort(int[] array) {
+        System.out.println("Executing the Bubble Sort method");
+
+        long startTimeMillis = System.currentTimeMillis();
+
+        for(int sortedIndex = 0; sortedIndex < array.length; sortedIndex++) {
+
+            int smallestElement = sortedIndex;
+
+            for(int queryIndex = sortedIndex + 1; queryIndex < array.length; queryIndex++) {
+                if (array[queryIndex] < array[smallestElement]) {
+                    smallestElement = queryIndex;
+                }
+            }
+
+            int temporaryValue = array[sortedIndex];
+
+            array[sortedIndex] = smallestElement;
+            array[smallestElement] = temporaryValue;
+        }
+
+        long endTimeMillis = System.currentTimeMillis();
+        long elapsedTimeMillis = endTimeMillis - startTimeMillis;
+
+        return elapsedTimeService.formatElapsedTime(elapsedTimeMillis);
+    }
+}
