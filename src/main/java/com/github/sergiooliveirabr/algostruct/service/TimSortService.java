675ed0427de32dc6972a377b3fca8357db1947c9
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-07 18:05:32 +0100

    Create TimSortService.java
    act:
    -> It is a method of the utility class java.util.Arrays

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/TimSortService.java
@@ -0,0 +1,32 @@
+package com.github.sergiooliveirabr.algostruct.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Service
+public class TimSortService  implements SortingAlgorithm {
+
+    private final ElapsedTimeService elapsedTimeService;
+
+    @Autowired
+    public TimSortService(ElapsedTimeService elapsedTimeService) {
+        this.elapsedTimeService = elapsedTimeService;
+    }
+
+    @Override
+    public long sort(int[] array) {
+
+        long startTimeMillis = System.currentTimeMillis();
+
+        //Arrays of int, double etc. use a modern variant of QuickSort called DualPivotQuickSort.
+        Arrays.sort(array);
+
+        long endTimeMillis = System.currentTimeMillis();
+        long elapsedTimeMillis = endTimeMillis - startTimeMillis;
+
+        return elapsedTimeService.formatElapsedTime(elapsedTimeMillis);
+    }
+}
