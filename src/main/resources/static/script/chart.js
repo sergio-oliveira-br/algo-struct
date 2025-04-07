a36de2da87249d17603389834f2145df036fa25e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-07 17:54:28 +0100

    Update chart.js
    act:
    -> approach changed. From now on, the data will invoke in the HTML by using the correct approach from thymeleaf

--- src/main/resources/static/script/chart.js
+++ src/main/resources/static/script/chart.js
@@ -1,19 +1,10 @@
 
-
-
-
-
 const ctx = document.getElementById('myChart');
-
-// let labels = /*[[${labels}]]*/ [];
-// let data = /*[[${qty}]]*/ [];
-
-let data = [2, 3, 4, 5, 6];
 
 const myChart = new Chart (ctx, {
     type: 'bar',
     data: {
-        // labels: labels,
+        labels: labels,
         datasets: [{
             data: data,
             label: "qty"
