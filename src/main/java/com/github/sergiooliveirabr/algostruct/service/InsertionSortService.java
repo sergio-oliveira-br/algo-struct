4926b6dc67da2204448fe34eadbcddf26acee634
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-08 10:12:30 +0100

    Create InsertionSortService.java
    act:
    -> method implemented

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/InsertionSortService.java
@@ -0,0 +1,41 @@
+package com.github.sergiooliveirabr.algostruct.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class InsertionSortService implements  SortingAlgorithm {
+
+    private final ElapsedTimeService elapsedTimeService;
+
+    @Autowired
+    public InsertionSortService(ElapsedTimeService elapsedTimeService) {
+        this.elapsedTimeService = elapsedTimeService;
+    }
+
+
+    @Override
+    public long sort(int[] array) {
+
+        System.out.println("Executing the Insertion Sort method");
+
+        long startTimeMillis = System.currentTimeMillis();
+
+        for(int i = 1; i < array.length; i++) {
+            int currentValue = array[i];
+            int position = i - 1;
+
+            while(position >= 0 && array[position] > currentValue) {
+                array[position + 1 ] = array[position];
+                position--;
+            }
+            array[position + 1] = currentValue;
+        }
+        long endTimeMillis = System.currentTimeMillis();
+        long elapsedTimeMillis = endTimeMillis - startTimeMillis;
+
+        return elapsedTimeService.formatElapsedTime(elapsedTimeMillis);
+    }
+}
