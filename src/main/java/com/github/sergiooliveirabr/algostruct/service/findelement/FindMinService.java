7add6a6ad30c3b7c2cb729fe1e521db72affbc9a
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-08 22:04:23 +0100

    Create FindMinService.java

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/findelement/FindMinService.java
@@ -0,0 +1,18 @@
+package com.github.sergiooliveirabr.algostruct.service.findelement;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class FindMinService implements FindMinMaxInterface {
+
+    @Override
+    public int findMinMax(int[] arrayNumbers) {
+        System.out.println("\nExecuting the Find Min method");
+
+        Arrays.sort(arrayNumbers);
+
+        return arrayNumbers[0];
+    }
+}
