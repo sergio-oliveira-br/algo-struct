f02cbd0f94d8374ff813866a2c2d427e8cd29301
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-09 13:38:56 +0100

    Create ArraySortedService.java

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/duplicatecheck/ArraySortedService.java
@@ -0,0 +1,23 @@
+package com.github.sergiooliveirabr.algostruct.service.duplicatecheck;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class ArraySortedService implements DuplicateCheckInterface {
+
+    @Override
+    public boolean isDuplicate(int[] arrayNumbers) {
+
+        Arrays.sort(arrayNumbers);
+
+        for(int i = 0; i < arrayNumbers.length - 1; i++) {
+            if (arrayNumbers[i] == arrayNumbers[i + 1]) {
+                System.out.println( arrayNumbers[i] + ", " + arrayNumbers[i + 1]);
+                return true;
+            }
+        }
+        return false;
+    }
+}
