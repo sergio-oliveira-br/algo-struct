8edb7fa1c6e0c92a737b4a2a9931282da9fc37f7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-09 13:39:05 +0100

    Create DuplicateCheckService.java

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/duplicatecheck/DuplicateCheckService.java
@@ -0,0 +1,36 @@
+package com.github.sergiooliveirabr.algostruct.service.duplicatecheck;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DuplicateCheckService {
+
+    private final Map<String, DuplicateCheckInterface> duplicateCheckMap;
+
+    @Autowired
+    public DuplicateCheckService(Map<String, DuplicateCheckInterface> duplicateCheckMap) {
+        this.duplicateCheckMap = duplicateCheckMap;
+    }
+
+    public Map<String, Boolean> executeDuplicateCheckAlgorithms(int[] arrayNumbers,
+                                                                List<String> algorithmSelected) {
+
+        System.out.println("\n[Service Class] - Available keys in duplicateCheckMap:" + duplicateCheckMap.keySet());
+        System.out.println("User has selected: " + algorithmSelected);
+
+        Map<String, Boolean> result = new HashMap<>();
+        for (String key: algorithmSelected) {
+            DuplicateCheckInterface duplicateCheckInterface = duplicateCheckMap.get(key);
+
+            if (duplicateCheckInterface != null) {
+                result.put(key, duplicateCheckInterface.isDuplicate(arrayNumbers.clone()));
+            }
+        }
+        return result;
+    }
+}
