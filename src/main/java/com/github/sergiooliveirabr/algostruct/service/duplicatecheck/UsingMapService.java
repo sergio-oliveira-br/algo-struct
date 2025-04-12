982725d9edd28376e87f4de3eddb3a4031d177a8
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-12 09:50:08 +0100

    Create UsingMapService.java
    Act:
    -> Iterate through the array, adding each element to a HashSet. If an element is already present, it indicates a duplicate.

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/duplicatecheck/UsingMapService.java
@@ -0,0 +1,28 @@
+package com.github.sergiooliveirabr.algostruct.service.duplicatecheck;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UsingMapService implements DuplicateCheckInterface {
+
+    @Override
+    public boolean isDuplicate(int[] arrayNumbers) {
+
+        System.out.println("\nExecuting the HashMap method");
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int number : arrayNumbers) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+
+            if(map.get(number) != 1) {
+                System.out.println("Duplicate key: " + number);
+                return true;
+            }
+        }
+        return false;
+    }
+}
