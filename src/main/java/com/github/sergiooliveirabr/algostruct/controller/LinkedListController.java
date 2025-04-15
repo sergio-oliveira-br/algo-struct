7835daeaea0ee6cfe7cc084d0b4c5d0e07ca2cb4
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-15 15:32:03 +0100

    Update LinkedListController.java

--- src/main/java/com/github/sergiooliveirabr/algostruct/controller/LinkedListController.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/controller/LinkedListController.java
@@ -1,5 +1,7 @@
 package com.github.sergiooliveirabr.algostruct.controller;
 
+import com.github.sergiooliveirabr.algostruct.service.linkedlist.InsertAtBeginningService;
+import com.github.sergiooliveirabr.algostruct.service.linkedlist.InserteAtEndService;
 import com.github.sergiooliveirabr.algostruct.service.linkedlist.SinglyLinkedListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,15 +11,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.Objects;
+
 @Controller
 @RequestMapping("/linked-list")
 public class LinkedListController {
 
     private final SinglyLinkedListService singlyLinkedListService;
+    private final InserteAtEndService<Integer> inserteAtEndService;
+    private final InsertAtBeginningService<Integer> insertAtBeginningService;
 
     @Autowired
-    public LinkedListController(SinglyLinkedListService singlyLinkedListService) {
+    public LinkedListController(SinglyLinkedListService singlyLinkedListService,
+                                InserteAtEndService<Integer> inserteAtEndService,
+                                InsertAtBeginningService<Integer> insertAtBeginningService) {
+
         this.singlyLinkedListService = singlyLinkedListService;
+        this.inserteAtEndService = inserteAtEndService;
+        this.insertAtBeginningService = insertAtBeginningService;
     }
 
     @GetMapping("/page")
@@ -27,10 +39,16 @@ public class LinkedListController {
 
 
     @PostMapping("/insert-at-end")
-    public String insertAndDisplay(Model model, @RequestParam int element) {
+    public String insertAndDisplay(Model model,
+                                   @RequestParam int element,
+                                   @RequestParam String algorithm) {
 
-        singlyLinkedListService.insertAtEnd(element);
-
+        if(Objects.equals(algorithm, "insertAtEndService")){
+            inserteAtEndService.insertElement(element);
+        }
+        else{
+            insertAtBeginningService.insertElement(element);
+        }
         model.addAttribute("myList", singlyLinkedListService.toString());
         return "linked-list";
     }
