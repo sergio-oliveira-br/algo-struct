af0bd686c554c0b099392063740e3a17bb358b8f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-15 20:54:46 +0100

    Update LinkedListController.java

--- src/main/java/com/github/sergiooliveirabr/algostruct/controller/LinkedListController.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/controller/LinkedListController.java
@@ -1,5 +1,6 @@
 package com.github.sergiooliveirabr.algostruct.controller;
 
+import com.github.sergiooliveirabr.algostruct.service.linkedlist.delete.DeleteFirstElementService;
 import com.github.sergiooliveirabr.algostruct.service.linkedlist.insert.InsertAtBeginningService;
 import com.github.sergiooliveirabr.algostruct.service.linkedlist.insert.InserteAtEndService;
 import com.github.sergiooliveirabr.algostruct.service.linkedlist.SinglyLinkedListService;
@@ -20,15 +21,17 @@ public class LinkedListController {
     private final SinglyLinkedListService singlyLinkedListService;
     private final InserteAtEndService<Integer> inserteAtEndService;
     private final InsertAtBeginningService<Integer> insertAtBeginningService;
+    private final DeleteFirstElementService<Integer> deleteFirstElementService;
 
     @Autowired
     public LinkedListController(SinglyLinkedListService singlyLinkedListService,
                                 InserteAtEndService<Integer> inserteAtEndService,
-                                InsertAtBeginningService<Integer> insertAtBeginningService) {
+                                InsertAtBeginningService<Integer> insertAtBeginningService, DeleteFirstElementService<Integer> deleteFirstElementService) {
 
         this.singlyLinkedListService = singlyLinkedListService;
         this.inserteAtEndService = inserteAtEndService;
         this.insertAtBeginningService = insertAtBeginningService;
+        this.deleteFirstElementService = deleteFirstElementService;
     }
 
     @GetMapping("/page")
@@ -52,6 +55,18 @@ public class LinkedListController {
         model.addAttribute("myList", singlyLinkedListService.toString());
         model.addAttribute("mySize",
                 "Size of the List: " + singlyLinkedListService.size());
+        return "linked-list";
+    }
+
+    @PostMapping("/delete-first-element")
+    public String deleteFirstElement(Model model) {
+
+        deleteFirstElementService.deleteElement();
+
+        model.addAttribute("myList", singlyLinkedListService.toString());
+        model.addAttribute("mySize",
+                "Size of the List: " + singlyLinkedListService.size());
+
         return "linked-list";
     }
 }
