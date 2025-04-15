603bdc097ddcc620c2ada15a8254e04109810ccc
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-15 20:54:41 +0100

    Create DeleteFirstElementService.java

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/linkedlist/delete/DeleteFirstElementService.java
@@ -0,0 +1,28 @@
+package com.github.sergiooliveirabr.algostruct.service.linkedlist.delete;
+
+import com.github.sergiooliveirabr.algostruct.service.linkedlist.SinglyLinkedListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteFirstElementService<T> implements DeleteElementStrategy {
+
+    private final SinglyLinkedListService<T> singlyLinkedListService;
+
+    @Autowired
+    public DeleteFirstElementService(SinglyLinkedListService<T> singlyLinkedListService) {
+        this.singlyLinkedListService = singlyLinkedListService;
+    }
+
+    @Override
+    public void deleteElement() {
+
+        if (!singlyLinkedListService.isEmpty()){
+            singlyLinkedListService.setHead(singlyLinkedListService.getHead().next);
+
+            if(singlyLinkedListService.getHead() == null){
+                singlyLinkedListService.setTail(null);
+            }
+        }
+    }
+}
