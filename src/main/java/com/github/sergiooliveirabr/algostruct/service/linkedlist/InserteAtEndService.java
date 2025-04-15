173c92d978ef0e1a10106c472c7cda1259ebad58
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-15 15:29:38 +0100

    Create InserteAtEndService.java

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/linkedlist/InserteAtEndService.java
@@ -0,0 +1,30 @@
+package com.github.sergiooliveirabr.algostruct.service.linkedlist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InserteAtEndService<T> implements InsertElementInterface<T> {
+
+    private final SinglyLinkedListService<T> singlyLinkedListService;
+
+    @Autowired
+    public InserteAtEndService(SinglyLinkedListService<T> singlyLinkedListService) {
+        this.singlyLinkedListService = singlyLinkedListService;
+    }
+
+    @Override
+    public void insertElement(T value) {
+
+        Node<T> newNode = new Node<>(value);
+
+        if(singlyLinkedListService.isEmpty()){
+            singlyLinkedListService.setHead(newNode);
+            singlyLinkedListService.setTail(newNode);
+        }
+        else{
+            singlyLinkedListService.getTail().next = newNode;
+            singlyLinkedListService.setTail(newNode);
+        }
+    }
+}
