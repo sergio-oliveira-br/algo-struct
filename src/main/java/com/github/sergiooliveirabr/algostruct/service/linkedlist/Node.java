2a35d91144e99e6d9b20a1363d3d39377ba23f89
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-18 19:26:37 +0100

    Update Node.java
    act:
    -> getters and setters added to make possible get the value for future comparison

--- src/main/java/com/github/sergiooliveirabr/algostruct/service/linkedlist/Node.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/linkedlist/Node.java
@@ -14,6 +14,22 @@ public class Node <T> {
         this(element, null);
     }
 
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
     @Override
     public String toString() {
         return element + "";
