909ea1458094a4be73a4ae85cf910a8aabc43d94
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-04-18 19:28:07 +0100

    Create FindByValueService.java
    act:
    -> this method will return true/false if the value inserted by the user is present in the Linked List

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/linkedlist/search/FindByValueService.java
@@ -0,0 +1,37 @@
+package com.github.sergiooliveirabr.algostruct.service.linkedlist.search;
+
+import com.github.sergiooliveirabr.algostruct.service.linkedlist.Node;
+import com.github.sergiooliveirabr.algostruct.service.linkedlist.SinglyLinkedListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FindByValueService<T> {
+
+    private final SinglyLinkedListService<T> singlyLinkedListService;
+
+    @Autowired
+    public FindByValueService(SinglyLinkedListService<T> singlyLinkedListService) {
+        this.singlyLinkedListService = singlyLinkedListService;
+    }
+
+    public boolean findByValeu(int value){
+
+        Node<T> head = singlyLinkedListService.getHead();
+        Node currentNode = head;
+
+        if(head == null){
+            return false;
+        }
+        else if (head.getElement().equals(value)) {
+            return true;
+        }
+        while (currentNode != null) {
+            if (currentNode.getElement().equals(value)) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+}
