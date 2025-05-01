d63124d5b66cf273e49a83fa66770878e774af95
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-01 18:05:35 +0100

    Update LinkedListController.java
    Method Refactored
    PGR Pattern implemented
    
    Act:
    -> The model attributes has been moved to GET view responsible to render the page.

--- src/main/java/com/github/sergiooliveirabr/algostruct/controller/LinkedListController.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/controller/LinkedListController.java
@@ -13,10 +13,8 @@ import com.github.sergiooliveirabr.algostruct.service.linkedlist.search.FindValu
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Objects;
 
@@ -63,8 +61,19 @@ public class LinkedListController {
     public String showLinkedListPage(Model model) {
 
         model.addAttribute("myList", singlyLinkedListService.toString());
-        model.addAttribute("mySize",
-                "Size of the List: " + singlyLinkedListService.size());
+        model.addAttribute("mySize", "Size of the List: " + singlyLinkedListService.size());
+
+        //This is related to Search for an Element
+        if(model.containsAttribute("value")) {
+
+            int value = (int) model.getAttribute("value");
+            boolean searchResult = (boolean) model.getAttribute("searchResult");
+
+            model.addAttribute("searchResult", searchResult);
+            model.addAttribute("nextNode", findNodeService.findNextNode(value));
+            model.addAttribute("node", "Searching for Node " + value);
+        }
+
         return "linked-list";
     }
 
@@ -95,21 +104,15 @@ public class LinkedListController {
     }
 
     @PostMapping("/find")
-    public String findByValue(Model model, @RequestParam int value) {
+    public String findByValue(@RequestParam int value, RedirectAttributes redirectAttributes) {
 
         boolean searchResult = findValueService.findByValue(value);
 
-        if (searchResult) {
-            findNodeService.findNextNode(value);
-        }
+        //This will send the info to view according the PRG Pattern
+        redirectAttributes.addFlashAttribute("searchResult", searchResult);
+        redirectAttributes.addFlashAttribute("value", value);
 
-        model.addAttribute("myList", singlyLinkedListService.toString());
-        model.addAttribute("mySize",
-                "Size of the List: " + singlyLinkedListService.size());
-        model.addAttribute("searchResult", searchResult);
-        model.addAttribute("nextNode", findNodeService.findNextNode(value));
-        model.addAttribute("node", "Searching for Node " + value);
-        return "linked-list";
+        return "redirect:/linked-list/page";
     }
 
     @PostMapping("/insert-at-given-index")
