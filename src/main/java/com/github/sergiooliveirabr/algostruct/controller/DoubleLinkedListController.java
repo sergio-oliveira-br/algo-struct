84a3d4ae925ef7073ffd3a991911210605f8cdc8
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-01 12:48:38 +0100

    Update DoubleLinkedListController.java
    act:
    -> endpoint "insert" created to invoke the methods responsible to insert the elements within the DLL. For now just one method was added which is "insert at the end"

--- src/main/java/com/github/sergiooliveirabr/algostruct/controller/DoubleLinkedListController.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/controller/DoubleLinkedListController.java
@@ -1,15 +1,40 @@
 package com.github.sergiooliveirabr.algostruct.controller;
 
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.DoubleLinkedListService;
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.insert.InsertAtEndDLLStrategy;
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.insert.InsertElementDLLStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/double-linked-list")
 public class DoubleLinkedListController {
 
+    private final DoubleLinkedListService<Integer> doubleLinkedListService;
+    private final InsertAtEndDLLStrategy<Integer> insertAtEndDLLStrategy;
+
+    @Autowired
+    public DoubleLinkedListController(DoubleLinkedListService<Integer> doubleLinkedListService, InsertAtEndDLLStrategy<Integer> insertAtEndDLLStrategy) {
+        this.doubleLinkedListService = doubleLinkedListService;
+        this.insertAtEndDLLStrategy = insertAtEndDLLStrategy;
+    }
+
     @GetMapping("/page")
     public String showDoubleLinkedListPage() {
         return "double-linked-list";
     }
+
+    @PostMapping("/insert")
+    public String insertElement(@RequestParam int element) {
+
+        insertAtEndDLLStrategy.insertElementDLL(element);
+        return "redirect:/double-linked-list/page";
+    }
+
+
 }
