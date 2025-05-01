ea256c10c53345fc50682f22303688188518425e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-01 12:44:20 +0100

    Update DoubleLinkedListService.java
    act:
    -> getAllElementsFromDLL implemented. This will make it possible to store all the elements within the array for printing in the UI.

--- src/main/java/com/github/sergiooliveirabr/algostruct/service/doublelinkedlist/DoubleLinkedListService.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/doublelinkedlist/DoubleLinkedListService.java
@@ -3,6 +3,9 @@ package com.github.sergiooliveirabr.algostruct.service.doublelinkedlist;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Service
 public class DoubleLinkedListService<T> {
@@ -17,5 +20,17 @@ public class DoubleLinkedListService<T> {
 
     public boolean isEmpty() {
        return headDLL == null;
+    }
+
+    public List<T> getAllElementsFromDLL(){
+
+        List<T> list = new ArrayList<>();
+        NodeDLL<T> current = headDLL;
+
+        while (current != null) {
+            list.add(current.getElement());
+            current = current.getNext();
+        }
+        return list;
     }
 }
