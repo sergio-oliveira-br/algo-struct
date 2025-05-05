12935b5882bb4aa1d3708675f54f91d2368b3589
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-05 10:31:37 +0100

    Create InsertAtBeginningDLL.java
    Act:
    -> method implemented
    Insert At Beginning Double Linked List

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/doublelinkedlist/insert/InsertAtBeginningDLL.java
@@ -0,0 +1,42 @@
+package com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.insert;
+
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.DoubleLinkedListService;
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.NodeDLL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InsertAtBeginningDLL implements InsertElementDLLStrategy {
+
+    private final DoubleLinkedListService<Integer> doubleLinkedListService;
+
+    @Autowired
+    public InsertAtBeginningDLL(DoubleLinkedListService<Integer> doubleLinkedListService) {
+        this.doubleLinkedListService = doubleLinkedListService;
+    }
+
+    @Override
+    public void insertElementDLL(int element) {
+
+        NodeDLL<Integer> newNodeDLL = new NodeDLL<>(element);
+
+        int lengthDDL = doubleLinkedListService.size();
+
+        if(lengthDDL == 0) {
+            doubleLinkedListService.setHeadDLL(newNodeDLL);
+            newNodeDLL.setPrevious(null);
+
+            doubleLinkedListService.setTailDLL(newNodeDLL);
+            newNodeDLL.setNext(null);
+        }
+        else if (lengthDDL > 1) {
+            NodeDLL<Integer> oldHeadDLL = doubleLinkedListService.getHeadDLL();
+
+            oldHeadDLL.setPrevious(newNodeDLL);
+            newNodeDLL.setNext(oldHeadDLL);
+
+            doubleLinkedListService.setHeadDLL(newNodeDLL);
+            newNodeDLL.setPrevious(null);
+        }
+    }
+}
