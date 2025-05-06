a8870604208fc1a9d401cb7314d972a3c2870d02
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-05-06 12:43:43 +0100

    Create InsertAtGivenIndexDLLStrategy.java
    Act:
    -> insert an Element By Index DLL implemented

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/doublelinkedlist/insert/InsertAtGivenIndexDLLStrategy.java
@@ -0,0 +1,51 @@
+package com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.insert;
+
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.DoubleLinkedListService;
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.NodeDLL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InsertAtGivenIndexDLLStrategy {
+
+    private final DoubleLinkedListService<Integer> doubleLinkedListService;
+    private final InsertAtBeginningDLLStrategy insertAtBeginningDLLStrategy;
+    private final InsertAtEndDLLStrategy insertAtEndDLLStrategy;
+
+    @Autowired
+    public InsertAtGivenIndexDLLStrategy(DoubleLinkedListService<Integer> doubleLinkedListService, InsertAtBeginningDLLStrategy insertAtBeginningDLLStrategy, InsertAtEndDLLStrategy insertAtEndDLLStrategy) {
+        this.doubleLinkedListService = doubleLinkedListService;
+        this.insertAtBeginningDLLStrategy = insertAtBeginningDLLStrategy;
+        this.insertAtEndDLLStrategy = insertAtEndDLLStrategy;
+    }
+
+    public void insertElementByIndexDLL(int element, int index) {
+
+        NodeDLL<Integer> newNodeDLL = new NodeDLL<>(element);
+        int lengthDLL = doubleLinkedListService.size();
+
+        if(lengthDLL == 0 || index == 0) {
+            insertAtBeginningDLLStrategy.insertElementDLL(newNodeDLL.getElement());
+        }
+        else if (index == lengthDLL) {
+            insertAtEndDLLStrategy.insertElementDLL(newNodeDLL.getElement());
+        }
+        else if (index > 0 && index < lengthDLL) {
+            NodeDLL<Integer> currentElement = doubleLinkedListService.getHeadDLL();
+            NodeDLL<Integer> previousCurrent = null;
+
+           for (int i = 0; i < index; i++) {
+               previousCurrent = currentElement;
+               currentElement = currentElement.getNext();
+           }
+            //Update the list
+            newNodeDLL.setNext(currentElement);
+            newNodeDLL.setPrevious(previousCurrent);
+            previousCurrent.setNext(newNodeDLL);
+            currentElement.setPrevious(newNodeDLL);
+        }
+        else {
+            throw new IllegalArgumentException("Sorry! Index out of bounds");
+        }
+    }
+}
