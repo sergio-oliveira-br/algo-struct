bb736627a1c6c93b9e3cb4e91ef2bb8372d3ab35
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-06-01 11:46:52 +0100

    Create DeleteLastElementDLLService.java
    act;
    -> business logic created to delete the last element from double linked list

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/doublelinkedlist/delete/DeleteLastElementDLLService.java
@@ -0,0 +1,47 @@
+package com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.delete;
+
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.DoubleLinkedListService;
+import com.github.sergiooliveirabr.algostruct.service.doublelinkedlist.NodeDLL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.EmptyStackException;
+
+@Service
+public class DeleteLastElementDLLService<T> implements DeleteElementStrategyDLL {
+
+    private final DoubleLinkedListService<T> doubleLinkedListService;
+
+    @Autowired
+    public DeleteLastElementDLLService(DoubleLinkedListService<T> doubleLinkedListService) {
+        this.doubleLinkedListService = doubleLinkedListService;
+    }
+
+    @Override
+    public void deleteElementDLL() {
+
+        NodeDLL<T> currentTail = doubleLinkedListService.getTailDLL();
+        NodeDLL<T> head = doubleLinkedListService.getHeadDLL();
+
+        if(doubleLinkedListService.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        else if (currentTail == head) {
+            currentTail.setPrevious(null);
+            currentTail.setNext(null);
+
+            doubleLinkedListService.setTailDLL(null);
+            doubleLinkedListService.setHeadDLL(null);
+        }
+        else {
+            NodeDLL<T> newTail = currentTail.getPrevious();
+
+            //disconnect
+            newTail.setNext(null);
+            currentTail.setPrevious(null);
+
+            //new set
+            doubleLinkedListService.setTailDLL(newTail);
+        }
+    }
+}
