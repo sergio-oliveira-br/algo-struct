06144d8a718ed2ec1176983101b71b8fb09f8fc2
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-06-05 09:45:43 +0100

    Update GlobalExceptionHandler.java

--- src/main/java/com/github/sergiooliveirabr/algostruct/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/exceptions/GlobalExceptionHandler.java
@@ -1,26 +1,29 @@
-//package com.github.sergiooliveirabr.algostruct.exceptions;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-//@ControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    private ResponseEntity<ProblemDetails> buildErrorResponse (HttpStatus status, String message) {
-//
-//        ProblemDetails problemDetails = new ProblemDetails(
-//                status.value(),
-//                message
-//        );
-//        return new ResponseEntity<>(problemDetails, status);
-//    }
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ProblemDetails> exception (Exception e) {
-//
-//        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-//        return buildErrorResponse(status, e.getMessage());
-//    }
-//}
+package com.github.sergiooliveirabr.algostruct.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmptyListException.class)
+    public String handleEmptyListException(EmptyListException ex,
+                                           Model model,
+                                           HttpServletRequest request) {
+
+        model.addAttribute("errorMessage", ex.getMessage());
+
+        String referer = request.getHeader("referer");
+        String requestURI = request.getRequestURI();
+
+        System.out.println("This is referer: " + referer);
+        System.out.println("This is the URI: " + requestURI);
+
+        if (referer != null && !referer.isEmpty()) {
+            return "redirect:" + referer;
+        }
+        return "error";
+    }
+}
