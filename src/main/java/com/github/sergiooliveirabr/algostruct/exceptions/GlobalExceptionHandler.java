09b362d9c98ebf220b20d42cee8cf3bb1212cc8d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-06-09 12:22:01 +0100

    Update GlobalExceptionHandler.java
    Act:
    -> method created

--- src/main/java/com/github/sergiooliveirabr/algostruct/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.github.sergiooliveirabr.algostruct.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -21,6 +20,21 @@ public class GlobalExceptionHandler {
 
         System.out.println("This is referer: " + referer);
         System.out.println("This is the URI: " + requestURI);
+
+        if (referer != null && !referer.isEmpty()) {
+            return "redirect:" + referer;
+        }
+        return "error";
+    }
+
+    @ExceptionHandler(InvalidIndexException.class)
+    public String handleInvalidIndexException(InvalidIndexException ex,
+                                              RedirectAttributes redirectAttributes,
+                                              HttpServletRequest request) {
+
+        redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+
+        String referer = request.getHeader("referer");
 
         if (referer != null && !referer.isEmpty()) {
             return "redirect:" + referer;
