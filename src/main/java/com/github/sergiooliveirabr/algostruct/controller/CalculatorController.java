6a0de0f37dfc334999f4d42917babd4a956309c2
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-02 14:36:57 +0100

    Create CalculatorController.java
    act:
    -> view created

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/controller/CalculatorController.java
@@ -0,0 +1,16 @@
+package com.github.sergiooliveirabr.algostruct.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    @GetMapping("/page")
+    public String showCalculatorPage(Model model) {
+        return "calculator";
+    }
+}
