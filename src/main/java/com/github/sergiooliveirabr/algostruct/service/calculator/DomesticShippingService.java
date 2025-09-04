57af232894622e977f006acccd2c0c6b5903ed57
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-04 17:27:15 +0100

    Create DomesticShippingService.java
    act:
    -> this method represent the domestic shipping which is current 12 as a fixed value

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/calculator/DomesticShippingService.java
@@ -0,0 +1,11 @@
+package com.github.sergiooliveirabr.algostruct.service.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DomesticShippingService {
+
+    public int domShippingCalculator(int domesticShippingfixedFee) {
+        return domesticShippingfixedFee;
+    }
+}
