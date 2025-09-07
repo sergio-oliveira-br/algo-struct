7de93c5ed697cea0503f6f76fb5453c72c41f354
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-07 12:54:58 +0100

    Update FinalCostDTO.java
    act:
    -> card payment variables added
    -> product price variable added

--- src/main/java/com/github/sergiooliveirabr/algostruct/dto/FinalCostDTO.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/dto/FinalCostDTO.java
@@ -9,14 +9,37 @@ import lombok.ToString;
 @ToString
 public class FinalCostDTO {
 
+    // Shipment
     private int internalShipping;
     private double externalShipping;
     private double shipmentCost;
 
+    // Card Payment Fee
+    private double serviceCharge;
+    private double plataformServiceFee;
 
-    public FinalCostDTO(int internalShipping, double externalShipping, double shipmentCost) {
+
+    // Product
+    private double productPrice;
+    private double productFinalPriceCYR;
+    private double productFinalPriceEUR;
+
+    public FinalCostDTO(int internalShipping,
+                        double externalShipping,
+                        double shipmentCost,
+                        double serviceCharge,
+                        double plataformServiceFee,
+                        double productPrice,
+                        double productFinalPriceCYR,
+                        double productFinalPriceEUR) {
+
         this.internalShipping = internalShipping;
         this.externalShipping = externalShipping;
         this.shipmentCost = shipmentCost;
+        this.serviceCharge = serviceCharge;
+        this.plataformServiceFee = plataformServiceFee;
+        this.productPrice = productPrice;
+        this.productFinalPriceCYR = productFinalPriceCYR;
+        this.productFinalPriceEUR = productFinalPriceEUR;
     }
 }
