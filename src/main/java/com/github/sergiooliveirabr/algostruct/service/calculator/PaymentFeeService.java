fdb08b0a305da11af57650550ad25d7d81264c72
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-07 12:56:28 +0100

    Create PaymentFeeService.java
    these methods are responsible to calculate the service charge and platform fee

---
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/calculator/PaymentFeeService.java
@@ -0,0 +1,27 @@
+package com.github.sergiooliveirabr.algostruct.service.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentFeeService {
+
+    private static final double PLATAFORM_RATE_PERCENTAGE = 1.025;
+    private static final double EXCHANGE_RATE_FEE_PERCENTAGE = 1.04; // 4%
+    private static final double CYR_FEE = 2.5; // 2.5 CYR
+
+    public double serviceChargeService(double productPrice) {
+
+        if(productPrice < 0) {
+            throw new IllegalArgumentException("Product Price must be greater than zero");
+        }
+        return productPrice * EXCHANGE_RATE_FEE_PERCENTAGE + CYR_FEE;
+    }
+
+    public double plataformServiceFeeService(double productPrice) {
+
+        if(productPrice < 0) {
+            throw new IllegalArgumentException("Product Price must be greater than zero");
+        }
+        return productPrice * PLATAFORM_RATE_PERCENTAGE;
+    }
+}
