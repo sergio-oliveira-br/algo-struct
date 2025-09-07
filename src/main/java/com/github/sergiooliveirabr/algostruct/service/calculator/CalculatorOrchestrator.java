be08f335383d8f3df48c7dc6ceca94dd1e9db99f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-07 12:57:15 +0100

    Update CalculatorOrchestrator.java
    act:
    -> payment fee service added
    -> currency conversion service added

--- src/main/java/com/github/sergiooliveirabr/algostruct/service/calculator/CalculatorOrchestrator.java
+++ src/main/java/com/github/sergiooliveirabr/algostruct/service/calculator/CalculatorOrchestrator.java
@@ -9,25 +9,50 @@ public class CalculatorOrchestrator {
 
     private final DomesticShippingService domesticShippingService;
     private final InternationalShippingService internationalShippingService;
+    private final PaymentFeeService paymentFeeService;
+    private final CurrencyConversionService currencyConversionService;
 
     @Autowired
     public CalculatorOrchestrator(DomesticShippingService domesticShippingService,
-                                  InternationalShippingService internationalShippingService) {
+                                  InternationalShippingService internationalShippingService,
+                                  PaymentFeeService paymentFeeService,
+                                  CurrencyConversionService currencyConversionService) {
 
         this.domesticShippingService = domesticShippingService;
         this.internationalShippingService = internationalShippingService;
+        this.paymentFeeService = paymentFeeService;
+        this.currencyConversionService = currencyConversionService;
     }
 
-    public FinalCostDTO calculator(int domesticShippingfixedFee, double productWeight) {
+    public FinalCostDTO calculator(int domesticShippingfixedFee, double productWeight, double productPrice) {
 
-        if (productWeight <= 0) {
-            throw new IllegalArgumentException("Produt Weight must be greater than zero");
+        if (productWeight <= 0 || productPrice <= 0 || domesticShippingfixedFee <= 0) {
+            throw new IllegalArgumentException("Weight, Price and Domestic Fee must be greater than zero");
         }
 
+        // Shipment Fee
         int domesticFee = domesticShippingService.domShippingCalculator(domesticShippingfixedFee);
         double internationalFee = internationalShippingService.intShippingCalculator(productWeight);
         double shipmentFee = domesticFee + internationalFee;
 
-        return new FinalCostDTO(domesticFee, internationalFee, shipmentFee);
+        // Payment Fee
+        double serviceCharge = paymentFeeService.serviceChargeService(productPrice);
+        double plataformServiceFee = paymentFeeService.plataformServiceFeeService(productPrice);
+
+        // Sum (¥)
+        double productFinalPriceCYR = shipmentFee + serviceCharge + plataformServiceFee;
+
+        // Conversion - CYR to EUR
+        double productFinalPriceEUR = productFinalPriceCYR / currencyConversionService.getExchangeRate("CNY");
+
+        return new FinalCostDTO(
+                domesticFee,
+                internationalFee,
+                shipmentFee,
+                serviceCharge,
+                plataformServiceFee,
+                productPrice,
+                productFinalPriceCYR,
+                productFinalPriceEUR);
     }
 }
